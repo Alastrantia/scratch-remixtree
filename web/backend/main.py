29a0ae61b467106c26d78cac9b50bc461a40567a
alastrantia <niklas.lulay@icloud.com>
2025-11-04 16:33:43 +0100

    should work on render now?

--- web/backend/main.py
+++ web/backend/main.py
@@ -173,8 +173,6 @@ async def get_tree(project_id: int, max_depth: int = None):
 async def health():
     return {"status": "healthy"}
 
-# for vercel
-app = app
 
 if __name__ == "__main__":
     import uvicorn
